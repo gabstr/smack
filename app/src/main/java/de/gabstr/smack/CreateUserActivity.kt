c129b759b52675a03e4ba459a81f50105403a9b5
gabstr <mnml@posteo.net>
2018-02-23 13:57:44 +0100

    lesson 04

---
+++ app/src/main/java/de/gabstr/smack/CreateUserActivity.kt
@@ -0,0 +1,25 @@
+package de.gabstr.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun generateBackgroundColorClicked(view: View) {
+
+    }
+
+    fun createUserBtnClicked(view: View) {
+
+    }
+}
