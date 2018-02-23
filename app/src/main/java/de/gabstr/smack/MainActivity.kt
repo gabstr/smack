0e6acc8fece989507079cb663d9e3559c68c6f6c
gabstr <mnml@posteo.net>
2018-02-23 13:41:28 +0100

    lesson 03

--- app/src/main/java/de/gabstr/smack/MainActivity.kt
+++ app/src/main/java/de/gabstr/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package de.gabstr.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -34,7 +35,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked(view: View) {
-
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
     }
 
     fun addChannelClicked(view: View) {
@@ -44,4 +46,6 @@ class MainActivity : AppCompatActivity() {
     fun sendMessageBtnClicked(view: View) {
 
     }
+
+
 }
