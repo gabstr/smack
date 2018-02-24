13895d5481118d1603448ec7064bcda938838508
gabstr <mnml@posteo.net>
2018-02-24 20:09:27 +0100

    lesson 18

--- app/src/main/java/de/gabstr/smack/controller/MainActivity.kt
+++ app/src/main/java/de/gabstr/smack/controller/MainActivity.kt
@@ -8,7 +8,6 @@ import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.view.CollapsibleActionView
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
@@ -16,11 +15,15 @@ import de.gabstr.smack.R
 import de.gabstr.smack.services.AuthService
 import de.gabstr.smack.services.UserDataService
 import de.gabstr.smack.utilities.BROADCAST_USER_DATA_CHANGE
+import de.gabstr.smack.utilities.SOCKET_URL
+import io.socket.client.IO
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val socket = IO.socket(SOCKET_URL)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,11 +34,23 @@ class MainActivity : AppCompatActivity() {
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+    }
 
-        hideKeyboard()
-
+    override fun onResume() {
         LocalBroadcastManager.getInstance(this)
                 .registerReceiver(userDataChangeReceiver, IntentFilter(BROADCAST_USER_DATA_CHANGE))
+        socket.connect()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(userDataChangeReceiver)
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        socket.disconnect()
+        super.onDestroy()
     }
 
     private val userDataChangeReceiver = object: BroadcastReceiver() {
@@ -89,18 +104,17 @@ class MainActivity : AppCompatActivity() {
                         val channelName = nameTextField.text.toString()
                         val channelDesc = descTextField.text.toString()
 
-                        hideKeyboard()
+                        socket.emit("newChannel", channelName, channelDesc)
                     }
                     .setNegativeButton("Cancel") {dialog, which ->
                         // Cancel and close the dialog
-                        hideKeyboard()
                     }
                     .show()
         }
     }
 
     fun sendMessageBtnClicked(view: View) {
-
+        hideKeyboard()
     }
 
     fun hideKeyboard() {
--- app/src/main/java/de/gabstr/smack/utilities/Constants.kt
+++ app/src/main/java/de/gabstr/smack/utilities/Constants.kt
@@ -2,6 +2,7 @@ package de.gabstr.smack.utilities
 
 const val BASE_URL = "https://chattymnml.herokuapp.com/v1/"
 //const val BASE_URL = "http://10.0.2.2:3005/v1/"
+const val SOCKET_URL = "https://chattymnml.herokuapp.com/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
 const val URL_CREATE_USER = "${BASE_URL}user/add"
